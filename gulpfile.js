976769ff1565604fcd7dafcb1a1fdc32d27a63e1
Patrick Vieira Feitosa <31283687+patrickfeitosa@users.noreply.github.com>
2018-05-06 20:47:52 -0300

    Subindo o código inicial com alguns parametros de Gulp já estabelecidos
    Subindo o código inicial com alguns parametros de Gulp já estabelecidos

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,11 @@
+//Definição dos Modulos a serem utilizados no projeto
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 const browserSync = require('browser-sync').create();
 
+
+//Função para complilar o SASS e adicionar os prefixos
 function compilaSass(){
     return gulp.src('css/scss/*.scss')
     .pipe(sass({
@@ -16,8 +19,12 @@ function compilaSass(){
     .pipe(browserSync.stream());
 }
 
+
+//Tarefa de Gulp para a função de SASS
 gulp.task('sass', compilaSass);
 
+
+//Função para iniciar o Browser
 function browser(){
     browserSync.init({
         server:{
@@ -26,12 +33,21 @@ function browser(){
     })
 }
 
+
+//Tarefa de Gulp para o Browser-sync
 gulp.task('browser-sync', browser);
 
+
+//Função de Watch do Gulp
 function watch(){
     gulp.watch('css/scss/*.scss', compilaSass);
+    gulp.watch(['*.html', '*.php']).on('change', browserSync.reload);
 }
 
+
+//Inicia a tarefa de Watch
 gulp.task('watch', watch);
 
+
+//Tarefa padrão do Gulp que inicia o Watch e browser-sync
 gulp.task('default', gulp.parallel('watch', 'browser-sync'));
